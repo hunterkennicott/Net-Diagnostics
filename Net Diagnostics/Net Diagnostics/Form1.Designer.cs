1759ff8741fac280f869a7a6f19b5f6240d07c85
hunterkennicott <HunterWalker23@gmail.com>
2019-12-16 20:29:23 -0700

    Initial upload; still encountering runtime errors with TTL and the display of progress bar.
    Everything added to a background worker.

---
+++ Net Diagnostics/Net Diagnostics/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Net_Diagnostics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(118, 237);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(188, 44);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Times New Roman", 55F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(80, 22);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(480, 84);
+            this.title.TabIndex = 3;
+            this.title.Text = "Net Diagnostic";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(335, 237);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(188, 44);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.progressBar1.Location = new System.Drawing.Point(118, 327);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(405, 34);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(139, 293);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(86, 31);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 117);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Click on the start\r\nbutton below to\r\nrun a network test";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(640, 373);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DBC Net Diagnostic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Net Diagnostics/Net Diagnostics/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Configuration;
+using System.Threading;
+using System.Management;
+using System.Net.Mail;
+
+namespace Net_Diagnostics
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bkWorker;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bkWorker = new BackgroundWorker();
+            bkWorker.DoWork += new DoWorkEventHandler(bkWorker_DoWork);
+            bkWorker.ProgressChanged += new ProgressChangedEventHandler(bkWorker_ProgressChanged);
+            bkWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bkWorker_RunWorkerCompleted);
+            bkWorker.WorkerReportsProgress = true;
+            bkWorker.WorkerSupportsCancellation = true;
+
+            lblStatus.Hide();
+            progressBar1.Hide();
+        }
+
+        void bkWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lblStatus.Text = "Task Cancelled";
+            }
+            else if (e.Error != null)
+            {
+                lblStatus.Text = "Error while performing background operation.";
+            }
+            else
+            {
+                lblStatus.Text = "Completed . . . . . ." + progressBar1.Value.ToString() + "%";
+                MessageBox.Show("Diagnostic complete. The file is saved on the Desktop as 'Diagnostic Results'");
+            }
+
+            startButton.Enabled = true;
+            btnCancel.Enabled = false;
+        }
+
+        void bkWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblStatus.Text = "Processing . . . . . ." + progressBar1.Value.ToString() + "%";
+        }
+
+        void emailDBC()
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("dbcnetdiag@gmail.com");
+            mail.To.Add("hunter@dbcontrols.com");
+            mail.Subject = "Connection Issues";
+            mail.Body = "Attached is the diagnostic results of a network";
+            //Attach txt file here
+
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("dbcnetdiag@gmail.com", "Dbc623Slc");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+            
+        }
+
+        void bkWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Ping pinger = new Ping();
+            //for speeds
+            PingReply replyPing = pinger.Send("65.100.224.20");
+            //for traceroute
+            string ipAddressOrHostName = "65.100.224.20";
+
+            //Create file on desktop and a way to write to it
+            string targetFolder = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
+            string targetPath = Path.Combine(targetFolder, "Diagnostic Results.txt");
+            StreamWriter sw = new StreamWriter(targetPath);
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Pinging DBC Server\r\n\r\n");
+            
+            //Basic info
+            sb.Append("Address: " + replyPing.Address + "\r\n");
+            sb.Append("Roundtrip Time: " + replyPing.RoundtripTime + "\r\n");
+            sb.Append("TTL (Time to Live): " + replyPing.Options.Ttl + "\r\n");
+            sb.Append("Buffer Size: " + replyPing.Buffer.Length.ToString() + "\r\n");
+            sb.Append("Time: " + DateTime.Now + "\r\n\r\n");
+
+            //Check Gateway
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if (!(bool)objMO["ipEnabled"])
+                    continue;
+
+                string[] gateways = (string[])objMO["DefaultIPGateway"];
+
+                //sb.AppendLine("Printing Default Gateway Info:");
+                //sb.AppendLine(objMO["DefaultIPGateway"].ToString());
+
+                sb.Append("Printing IPGateway info: ");
+                foreach (string sGate in gateways)
+                    sb.AppendLine(sGate);
+            }
+
+            //Check if IP is local
+            sb.Append("Is the IP local: ");
+            if (IsLocalIpAddress(System.Environment.MachineName) == true)
+            {
+                sb.Append("Yes\r\n\r\n");
+            }
+            else
+            {
+                sb.Append("No\r\n\r\n");
+            }
+
+            PingReply reply = pinger.Send(ipAddressOrHostName);
+
+            IPAddress ipAddress = Dns.GetHostEntry(ipAddressOrHostName).AddressList[0];
+            StringBuilder traceResults = new StringBuilder();
+
+            //Start tracerouting
+            using (Ping pingSender = new Ping())
+            {
+                PingOptions pingOptions = new PingOptions();
+                Stopwatch stopWatch = new Stopwatch();
+                byte[] bytes = new byte[32];
+
+                pingOptions.DontFragment = true;
+                pingOptions.Ttl = 1;
+                int maxHops = 30;
+
+                sb.AppendLine(string.Format("Tracing route to {0} over a maximum of {1} hops:", ipAddress, maxHops));
+                sb.AppendLine();
+
+                //Loop tracert and time it
+                for (int i = 1; i < maxHops + 1; i++)
+                {
+                    stopWatch.Reset();
+                    stopWatch.Start();
+                    PingReply pingReply = pingSender.Send(ipAddress, 5000, new byte[32], pingOptions);
+
+                    stopWatch.Stop();
+
+                    sb.AppendLine(string.Format("{0}\t{1} ms\t{2}", i, stopWatch.ElapsedMilliseconds, pingReply.Address));
+
+                    //break loop here
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        traceResults.AppendLine();
+                        traceResults.AppendLine("Trace complete.");
+
+                        break;
+                    }
+
+                    pingOptions.Ttl++;
+
+                    //Show progress after each hop
+                    bkWorker.ReportProgress(i);
+
+                    if (bkWorker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        bkWorker.ReportProgress(0);
+                        return;
+                    }
+                }
+            }
+
+            string results = sb.ToString();
+            sw.Write(sb);
+            sw.Close();
+
+            bkWorker.ReportProgress(100);
+        }
+
+        public static IPAddress GetDefaultGateway()
+        {
+            var card = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+            if (card == null) return null;
+            var address = card.GetIPProperties().GatewayAddresses.FirstOrDefault();
+            return address.Address;
+        }
+
+        public static bool IsLocalIpAddress(string host)
+        {
+            try
+            {
+                IPAddress[] hostIPs = Dns.GetHostAddresses(host);
+                IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+
+                foreach (IPAddress hostIP in hostIPs)
+                {
+                    if (IPAddress.IsLoopback(hostIP)) return true;
+                    foreach (IPAddress localIP in localIPs)
+                    {
+                        if (hostIP.Equals(localIP)) return true;
+                    }
+                }
+            }
+            catch { }
+            return false;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            btnCancel.Enabled = true;
+            bkWorker.RunWorkerAsync();
+
+            lblStatus.Show();
+            progressBar1.Show();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (bkWorker.IsBusy)
+            {
+                bkWorker.CancelAsync();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
